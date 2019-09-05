3ea1d2b5193ec641626cda61bcd0d25d8977f058
wupengforIT <wupengforIT@163.com>
2019-09-05 18:35:24 +0800

    同步代码

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import appVue from './router/app.vue';
 import VueRouter from 'vue-router';
-import router from './js/router.js'
+import router from './js/router.js';
 import VueResource from 'vue-resource';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
